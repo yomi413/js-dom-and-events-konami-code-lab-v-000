3d80d69ba1bbb2161295d91bb29a926dd48e94f9
Yomaira Escano <yjescano@gmail.com>
2018-09-11 05:07:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,18 +2,24 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0
 
-function init(event) {
+function init() {
   // Write your JavaScript code inside the init() function
-  const key = parseInt(event.detail || event.which);
-
-  if (key === code[index]) {
-    ++index
-    if (index === code.length) {
-      alert("All sytems passing!")
-
-      index = 0
-    }
-  } else {
-    index = 0
-  }
+  const doc = document.body
+  
+  doc.addEventListener('keydown', function(event) {
+    return alert('Success!')
+  })
+  
+  // const key = parseInt(event.detail || event.which);
+  // 
+  // if (key === code[index]) {
+  //   ++index
+  //   if (index === code.length) {
+  //     alert("All sytems passing!")
+  // 
+  //     index = 0
+  //   }
+  // } else {
+  //   index = 0
+  // }
 }
