54b2259376a38ebc4c4d076dd5599ff1491bba85
Yomaira Escano <yjescano@gmail.com>
2018-09-11 05:17:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,13 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-// let index = 0
+let index = 0
 
 function init() {
   // Write your JavaScript code inside the init() function
   const body = document.querySelector('body')
 
   body.addEventListener('keydown', function(event) {
-    return alert('Success!')
+    const key = parseInt(event.detail || event.which)
+
+    if (key === code[index]) {
+      ++index
+      if (index === code.length) {
+        alert('Success!')
+      }
+    } else {
+      index = 0
+    }
   })
 
   // const key = parseInt(event.detail || event.which);
