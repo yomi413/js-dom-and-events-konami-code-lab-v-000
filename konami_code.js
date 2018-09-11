df0e51265069d85ed7e35fce9b1dc558c96eb453
Yomaira Escano <yjescano@gmail.com>
2018-09-11 04:46:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let index = 0
 
+function init(event) {
+  // Write your JavaScript code inside the init() function
+  const key = parseInt(event.detail || event.which);
+
+  if (key === code[index]) {
+    ++index
+    if (index === code.length) {
+      alert("All sytems passing!")
+
+      index = 0
+    } 
+  } else {
+    index = 0
+  }
 }
