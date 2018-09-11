4bc1544381e743a52a702819ff9bab2bb2d5cbee
Yomaira Escano <yjescano@gmail.com>
2018-09-11 05:11:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,9 +4,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const doc = document.body
+  const body = document.querySelector('body')
 
-  doc.addEventListener('keydown', function(event) {
+  body.addEventListener('keydown', function(event) {
     return alert('Success!')
   })
 
